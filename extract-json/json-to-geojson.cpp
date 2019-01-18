1bd88a974fbd6d21e0ed189be0f394d948cefafd
Hugo Lucas <hlucas002@enseirb-matmeca.fr>
2019-01-18 15:25:07 +0100

    Change Points in LineString

--- extract-json/json-to-geojson.cpp
+++ extract-json/json-to-geojson.cpp
@@ -3,48 +3,26 @@
 #include <fstream>
 #include <iomanip>
 #include <string>
-#include "curl/curl.h"
 #include <map>
 
 using json = nlohmann::json;
+json coordinates = json::array();
 
-json convert_single_response(json j_input) {
-
-  json j_output = json::array();
-
+void convert_single_response(json j_input) {
   auto legs = j_input["routes"][0]["legs"];
   size_t size = legs.size();
 
   for (int i = 0; i < size; i++){
-
     auto steps = legs[i]["steps"];
     size_t size_steps = steps.size();
 
-
     for (int j = 0; j < size_steps; j++){
-
-      auto location = steps[j]["intersections"][0]["location"];
-
-      j_output.push_back({ {"type", "Feature"},
-        { "properties", {
-          {"Layer", "route"},
-          {"SubClasses", NULL},
-          {"ExtendedEntity", NULL},
-          {"Linetype", NULL},
-          {"EntityHandle", NULL}
-        }}, {"geometry", { {"type", "Point"}, {"coordinates", location} } } });
-
+      coordinates.push_back(steps[j]["intersections"][0]["location"]);
     }
-
   }
-
-  return j_output;
-
-  
 }
 
 int main(int argc, char** argv){
-
 
   if(argc != 3){
     std::cout << "Usage: " << argv[0] << " route-response.json json-to-geojson.geojson\n";
@@ -56,23 +34,28 @@ int main(int argc, char** argv){
   json j_input;
   input >> j_input;
 
-  std::ofstream output(argv[2], std::ofstream::out);
-  json j_output;      
-
   if (j_input.is_array()) {
     for ( size_t i = 0; i < j_input.size(); ++i ){
-
-      j_output.push_back(convert_single_response(j_input[i]));
+      convert_single_response(j_input[i]);
     }
   } else {
-    j_output.push_back(convert_single_response(j_input));
+    convert_single_response(j_input);
   }
 
-  json tmp = { {"type", "FeatureCollection"},
-  {"name", "capteurs"},{"features", j_output} };
+  json json_output = {
+      {"type", "Feature"},
+      {"properties", {
+        {"stroke", "#0000ff"},
+        {"stroke-width", 2},
+        {"stroke-opacity", 3}
+      }},
+      {"geometry", {
+        {"type", "LineString"},
+        {"coordinates", coordinates}
+      }}
+  };
 
-  output << std::setw(4) << tmp;
-
-  
+  std::ofstream output(argv[2], std::ofstream::out);
+  output << std::setw(4) << json_output;
 
 }
